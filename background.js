71aa6243f95f4efcdb53ce4ef37e2b7c9e49eb15
1 <1@1.com>
2023-02-25 01:49:36 +0800

    init

---
+++ background.js
@@ -0,0 +1,70 @@
+const rules = [
+    {
+        id: 1,
+        priority: 1,
+        action: {
+            type: "redirect",
+            redirect: {
+                regexSubstitution: "\\1://\\2.sinaimg.cn/\\3"
+            }
+        },
+        condition: {
+            regexFilter: "^([^\\.]*)://([^/]*)\\.moyu\\.im/(.*)$",
+            resourceTypes: [
+                "csp_report",
+                "font",
+                "image",
+                "main_frame",
+                "media",
+                "object",
+                "other",
+                "ping",
+                "script",
+                "stylesheet",
+                "sub_frame",
+                "webbundle",
+                "websocket",
+                "webtransport",
+                "xmlhttprequest"
+            ]
+        }
+    },
+    {
+        id: 2,
+        priority: 1,
+        action: {
+            type: "modifyHeaders",
+            requestHeaders: [
+                {
+                    "header": "referer",
+                    "operation": "set",
+                    "value": "https://weibo.com/"
+                }
+            ]
+        },
+        condition: {
+            "regexFilter": "^[^\\.]*://[^/]*\\.sinaimg\\.cn/.*$",
+            "resourceTypes": [
+                "csp_report",
+                "font",
+                "image",
+                "main_frame",
+                "media",
+                "object",
+                "other",
+                "ping",
+                "script",
+                "stylesheet",
+                "sub_frame",
+                "webbundle",
+                "websocket",
+                "webtransport",
+                "xmlhttprequest"
+            ]
+        }
+    }
+];
+chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: rules.map(r => r.id),
+    addRules: rules,
+});
