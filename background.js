e740782b605c936fab8ef7005e37eb2955862c23
1 <1@1.com>
2024-12-23 00:29:32 +0800

    support toto.im

--- background.js
+++ background.js
@@ -62,6 +62,36 @@ const rules = [
                 "xmlhttprequest"
             ]
         }
+    },
+    {
+        id: 3,
+        priority: 1,
+        action: {
+            type: "redirect",
+            redirect: {
+                regexSubstitution: "\\1://wx1.sinaimg.cn/\\2"
+            }
+        },
+        condition: {
+            regexFilter: "^([^\\.]*)://img\\.toto\\.im/(.*)$",
+            resourceTypes: [
+                "csp_report",
+                "font",
+                "image",
+                "main_frame",
+                "media",
+                "object",
+                "other",
+                "ping",
+                "script",
+                "stylesheet",
+                "sub_frame",
+                "webbundle",
+                "websocket",
+                "webtransport",
+                "xmlhttprequest"
+            ]
+        }
     }
 ];
 chrome.declarativeNetRequest.updateDynamicRules({
