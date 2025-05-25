60d3034a497772464589a9cc7b2fd1919be2eb8f
1 <1@1.com>
2025-05-25 22:39:22 +0800

    bugfix and new version support

--- background.js
+++ background.js
@@ -1,100 +1,109 @@
-const rules = [
-    {
-        id: 1,
-        priority: 1,
-        action: {
-            type: "redirect",
-            redirect: {
-                regexSubstitution: "\\1://\\2.sinaimg.cn/\\3"
+const load = async () => {
+    const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
+    const oldRuleIds = oldRules.map(rule => rule.id);
+    const rules = [
+        {
+            id: 1,
+            priority: 1,
+            action: {
+                type: "modifyHeaders",
+                requestHeaders: [
+                    {
+                        "header": "referer",
+                        "operation": "set",
+                        "value": "https://weibo.com/"
+                    }
+                ]
+            },
+            condition: {
+                "regexFilter": "^[^\\.]*://[^/]*\\.sinaimg\\.cn/.*$",
+                "resourceTypes": [
+                    "csp_report",
+                    "font",
+                    "image",
+                    "main_frame",
+                    "media",
+                    "object",
+                    "other",
+                    "ping",
+                    "script",
+                    "stylesheet",
+                    "sub_frame",
+                    "webbundle",
+                    "websocket",
+                    "webtransport",
+                    "xmlhttprequest"
+                ]
             }
         },
-        condition: {
-            regexFilter: "^([^\\.]*)://([^/]*)\\.moyu\\.im/(.*)$",
-            resourceTypes: [
-                "csp_report",
-                "font",
-                "image",
-                "main_frame",
-                "media",
-                "object",
-                "other",
-                "ping",
-                "script",
-                "stylesheet",
-                "sub_frame",
-                "webbundle",
-                "websocket",
-                "webtransport",
-                "xmlhttprequest"
-            ]
-        }
-    },
-    {
-        id: 2,
-        priority: 1,
-        action: {
-            type: "modifyHeaders",
-            requestHeaders: [
-                {
-                    "header": "referer",
-                    "operation": "set",
-                    "value": "https://weibo.com/"
+        {
+            id: 2,
+            priority: 2,
+            action: {
+                type: "redirect",
+                redirect: {
+                    regexSubstitution: "\\1://wx1.sinaimg.cn/\\2"
                 }
-            ]
-        },
-        condition: {
-            "regexFilter": "^[^\\.]*://[^/]*\\.sinaimg\\.cn/.*$",
-            "resourceTypes": [
-                "csp_report",
-                "font",
-                "image",
-                "main_frame",
-                "media",
-                "object",
-                "other",
-                "ping",
-                "script",
-                "stylesheet",
-                "sub_frame",
-                "webbundle",
-                "websocket",
-                "webtransport",
-                "xmlhttprequest"
-            ]
-        }
-    },
-    {
-        id: 3,
-        priority: 1,
-        action: {
-            type: "redirect",
-            redirect: {
-                regexSubstitution: "\\1://wx1.sinaimg.cn/\\2"
+            },
+            condition: {
+                regexFilter: "^([^\\.]*)://img\\.toto\\.im/(.*?)\\.webp$",
+                resourceTypes: [
+                    "csp_report",
+                    "font",
+                    "image",
+                    "main_frame",
+                    "media",
+                    "object",
+                    "other",
+                    "ping",
+                    "script",
+                    "stylesheet",
+                    "sub_frame",
+                    "webbundle",
+                    "websocket",
+                    "webtransport",
+                    "xmlhttprequest"
+                ]
             }
         },
-        condition: {
-            regexFilter: "^([^\\.]*)://img\\.toto\\.im/(.*)$",
-            resourceTypes: [
-                "csp_report",
-                "font",
-                "image",
-                "main_frame",
-                "media",
-                "object",
-                "other",
-                "ping",
-                "script",
-                "stylesheet",
-                "sub_frame",
-                "webbundle",
-                "websocket",
-                "webtransport",
-                "xmlhttprequest"
-            ]
+        {
+            id: 3,
+            priority: 2,
+            action: {
+                type: "redirect",
+                redirect: {
+                    regexSubstitution: "\\1://wx1.sinaimg.cn/\\2.gif"
+                }
+            },
+            condition: {
+                regexFilter: "^([^\\.]*)://img\\.toto\\.im/(.*?)\\.mp4$",
+                resourceTypes: [
+                    "main_frame",
+                ]
+            }
+        },
+        {
+            id: 4,
+            priority: 1,
+            action: {
+                type: "redirect",
+                redirect: {
+                    regexSubstitution: "\\1://wx1.sinaimg.cn/\\2"
+                }
+            },
+            condition: {
+                regexFilter: "^([^\\.]*)://img\\.toto\\.im/(.*)$",
+                resourceTypes: [
+                    "image",
+                    "main_frame",
+                    "sub_frame",
+                ]
+            }
         }
-    }
-];
-chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: rules.map(r => r.id),
-    addRules: rules,
-});
+    ];
+    chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: oldRuleIds,
+        addRules: rules,
+    });
+}
+load();
